1117ce3594dae5bc91fb5607fa741089f36b0c4c
Fatakhov <ildar1969ildar@yandex.ru>
2019-12-19 15:24:31 +0600

    feat: дз24

--- js/main.js
+++ js/main.js
@@ -75,12 +75,75 @@ $(document).ready(function () {
       },
       userPhone: "Телефон обязателен",
       userEmail: {
-        required: "Обязательно укажите email",
-        email: "Введите в формате: name@domain.com"
+        required: "Заполните поле",
+        email: "Введите корректный email в формате: name@domain.com"
       }
     }
   });
 //маска для номера телефона
 
 $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
+//валидация для control__form
+$('.control__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    userPhone: "required"
+      
+  },
+  messages: {
+    userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв",
+      maxlength: "Имя не более 15 букв"
+    },
+    userPhone: "Телефон обязателен"
+    
+  }
+});
+//маска для номера телефона
+
+$('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
+
+// Валидация для footer__form
+$('.footer__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    userPhone: "required",
+    // compound rule
+    userQuestion: {
+      required: true,
+      maxlength: 50
+    }
+  },
+  messages: {
+    userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв",
+      maxlength: "Имя не более 15 букв"
+    },
+    userPhone: "Телефон обязателен",
+    userQuestion: {
+      required: "Заполните поле",
+      maxlength: "Сообщение не более 50 знаков"
+    }
+  }
+});
+//маска для номера телефона
+
+$('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
 });
